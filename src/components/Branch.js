d3aea915d429b0a7f3c35dad83d23a3c8aa3dfa7
jchaasis <jchaasis@gmail.com>
2018-07-31 15:00:06 -0400

    Connect to the Node.js server.

--- src/components/Branch.js
+++ src/components/Branch.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 //import components
 import Leaf from './Leaf.js';
 
+
 class Branch extends Component {
+
+//call to the server to connect the data
+  componentDidMount(){
+    fetch("http://localhost:5000/", {
+        method: 'get'
+        // mode: 'no-cors'
+      })
+        .then(resp=> resp.json())
+        .then(resp=>console.log(resp))
+  }
+
   render() {
     return (
       <div className="branchContainer">
