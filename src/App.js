7144088e18d92ff806679a48a7f512e88e6325d6
jchaasis <jchaasis@gmail.com>
2018-07-31 15:57:16 -0400

    Add form input fields.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 
 //import components
 import Branch from './components/Branch.js';
-
+import Form from './components/Form.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Tree View</h1>
+          <h1 className="App-title topHalf">Live Action</h1>
           <hr id="dashes"/>
+          <h1 className="App-title bottomHalf">Tree View</h1>
         </header>
         <div>
           <h1 id="rootLabel"> Root </h1>
+          <Form/>
           <Branch/>
         </div>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name : null,
+            children : null,
+            min : null,
+            max : null,
+        }
+    }
   render() {
     return (
       <div className="formContainer">
-
+        <label> Name </label>
+        <input type="text"/>
+        <label> Children </label>
+        <input type="number" max="15" min="0"/>
+        <label> Min Range</label>
+        <input type="number"/>
+        <label> Max Range</label>
+        <input type="number"/>
+        <button> Add </button>
       </div>
     );
   }
