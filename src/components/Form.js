5ae0bdca73c4dcaca3169d2d12df785e53a42c4e
jchaasis <jchaasis@gmail.com>
2018-07-31 16:06:07 -0400

    Form data stores in component state.

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,21 +11,48 @@ class Form extends Component {
             max : null,
         }
     }
-  render() {
-    return (
-      <div className="formContainer">
-        <label> Name </label>
-        <input type="text"/>
-        <label> Children </label>
-        <input type="number" max="15" min="0"/>
-        <label> Min Range</label>
-        <input type="number"/>
-        <label> Max Range</label>
-        <input type="number"/>
-        <button> Add </button>
-      </div>
-    );
-  }
+
+    //update the state with the appropriate form data
+    addName(ev){
+        this.setState({
+            name: ev.target.value,
+        })
+    }
+    addChildren(ev){
+        this.setState({
+            children: ev.target.value,
+        })
+    }
+    addMin(ev){
+        this.setState({
+            min: ev.target.value,
+        })
+    }
+    addMax(ev){
+        this.setState({
+            max: ev.target.value,
+        })
+    }
+    //verify that all the fields have been completed then send the info to the backend
+    sendData(){
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+        <div className="formContainer">
+            <label> Name </label>
+            <input type="text" onChange={ev=>this.addName(ev)}/>
+            <label> Children </label>
+            <input type="number" max="15" min="0" onChange={ev=>this.addChildren(ev)}/>
+            <label> Min Range</label>
+            <input type="number" onChange={ev=>this.addMin(ev)}/>
+            <label> Max Range</label>
+            <input type="number" onChange={ev=>this.addMax(ev)}/>
+            <button onClick={()=>this.sendData()}> Add </button>
+        </div>
+        );
+    }
 }
 
 export default Form;
